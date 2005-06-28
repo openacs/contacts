2729088c08ffb2c4fb5b561f611b11f0d59e785a
Matthew Geddert <openacs@geddert.com>
2005-06-28 05:49:39 +0000

    Added the ability to remove contacts from groups and delete contacts

--- lib/groups.adp
+++ lib/groups.adp
@@ -7,11 +7,11 @@
 </else>
 </if>
 <if @groups:rowcount@ gt 0>
-<h3 class="contact-title"><a href="./groups">#contacts.Groups#</a></h3>
+<h3 class="contact-title"><if @hide_form_p@ true><a href="./groups"></if>#contacts.Groups#<if @hide_form_p@ true></a></if></h3>
 <ul>
 <multiple name="groups">
 <if @groups.sub_p@>(</if><else><li></else>
-@groups.group;noquote@
+@groups.group;noquote@ <a href="@groups.remove_url@"><img src="/resources/acs-subsite/Delete16.gif" width="16" height="16" border="0" alt="#contacts.Delete_from# @groups.group;noquote@"></a>
 <if @groups.sub_p@>)</if>
 </multiple>
 </ul>
@@ -20,3 +20,9 @@
 <strong>#contacts.lt_This_contact_is_not_p#</strong>
 </else>
 
+<if @hide_form_p@ false and @delete_p@>
+<h3>#contacts.Other_Options#</h3>
+<ul class="action-links">
+  <li><a href="@remove_url@">#contacts.lt_Delete_this_contact#</a>
+</ul>
+</if>
--- lib/groups.tcl
+++ lib/groups.tcl
@@ -14,7 +14,8 @@ set package_url [ad_conn package_url]
 
 set groups_belonging_to [db_list get_party_groups { select group_id from group_distinct_member_map where member_id = :party_id }]
 
-
+set return_url "[contact::url -party_id $party_id]groups"
+set object_type [contact::type -party_id $party_id]
 if { [string is false $hide_form_p] } {
     set group_options [list]
     set active_top_level ""
@@ -27,12 +28,13 @@ if { [string is false $hide_form_p] } {
     if { [llength $group_options] > 0 } {
         set group_options [concat [list [list "- [_ ams.select_one] -" ""]] $group_options]
         set package_url [ad_conn package_url]
+        set add_label [_ contacts.Add_to_Group]
         ad_form -name add_to_group -action "${package_url}group-party-add" \
             -form {
                     party_id:integer(hidden)
                     return_url:text(hidden),optional
                     {group_id:integer(select) {label {}} {options $group_options}}
-                    {save:text(submit),optional {label {Add to Group}}}
+                    {save:text(submit),optional {label {$add_label}}}
             } -on_request {
             } -after_submit {
             }
@@ -43,8 +45,7 @@ if { [string is false $hide_form_p] } {
 }
 
 
-
-multirow create groups group_id group sub_p
+multirow create groups group_id group sub_p remove_url
 set sub_p "0"
 foreach group [contact::groups -expand "all" -privilege_required "read"] {
     set group_name [lindex $group 0]
@@ -53,12 +54,18 @@ foreach group [contact::groups -expand "all" -privilege_required "read"] {
     } else {
 	set sub_p "0"
     }
+    set group_id [lindex $group 1]
+    set remove_url [export_vars -base "/contacts/group-parties-remove" -url {group_id party_id return_url}] 
     if { [lindex $group 2] == "1" } { set active_top_level [lindex $group 0] }
     if { [lsearch $groups_belonging_to [lindex $group 1]] >= 0 } {
-        multirow append groups [lindex $group 1] $group_name $sub_p
+        multirow append groups [lindex $group 1] $group_name $sub_p $remove_url
     }
 }
 
-
-
+if { [permission::permission_p -object_id "-2" -privilege "delete"] } {
+    set remove_url [export_vars -base "/contacts/group-parties-remove" -url {{group_id "-2"} party_id {return_url {/contacts/}}}] 
+    set delete_p 1
+} else {
+    set delete_p 0
+}
 
