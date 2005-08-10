27146953b5d863e4a197f37aed30abed4af7a29a
Malte Sussdorff <openacs@sussdorff.de>
2005-08-10 19:37:21 +0000

    Complaint support

---
+++ www/complaint.tcl
@@ -0,0 +1,22 @@
+ad_page_contract {
+
+    @author Matthew Geddert openacs@geddert.com
+    @creation-date 2005-07-09
+    @cvs-id $Id$
+
+
+} {
+    {party_id:integer}
+    {page "Complaints"}
+    {customer_id ""}
+} -validate {
+    contact_exists -requires {party_id} {
+	if { ![contact::exists_p -party_id $party_id] } {
+	    ad_complain "[_ contacts.lt_The_contact_specified]"
+	}
+    }
+}
+
+set supplier_id $party_id
+
+ad_return_template
