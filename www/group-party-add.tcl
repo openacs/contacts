dd55cd7dba42fd2e97d0fb331fe61e89076b2991
Malte Sussdorff <openacs@sussdorff.de>
2005-08-30 09:37:33 +0000

    Adding of groups works now for organizations as well

--- www/group-party-add.tcl
+++ www/group-party-add.tcl
@@ -10,8 +10,10 @@ ad_page_contract {
     {return_url ""}
 }
 
+
 set party_id [lindex $party_id 0]
-switch [contact::type -party_id $party_id] {
+set object_type [contact::type -party_id $party_id]
+switch $object_type {
     person {
 	set rel_type "membership_rel"
     }
@@ -22,7 +24,20 @@ switch [contact::type -party_id $party_id] {
 	set rel_type "membership_rel"
     }
 }
-relation_add -member_state "approved" $rel_type $group_id $party_id
+
+ns_log notice "\#\#\# rel_type $rel_type"
+if {$rel_type == "organization_rel"} {
+    set user_id [ad_conn user_id]
+    set ip_addr [ad_conn peeraddr]
+    set rel_id [db_exec_plsql add_organization_rel {}]
+    db_dml insert_state {}
+    # relation_add -member_state "approved" $rel_type $group_id $party_id
+} else {
+    group::add_member \
+	-group_id $group_id \
+	-user_id $party_id \
+	-rel_type membership_rel
+}
 
 if { ![exists_and_not_null return_url] } {
     set return_url[contact::url -party_id $party_id]
---
+++ www/group-party-add.xql
@@ -0,0 +1,10 @@
+<?xml version="1.0"?>
+<queryset>
+
+<fullquery name="insert_state">
+    <querytext>
+	insert into membership_rels (rel_id,member_state) values (:rel_id,'approved')
+    </querytext>
+</fullquery>
+
+</queryset>
