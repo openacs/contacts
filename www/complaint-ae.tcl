3f11f8001200b07ef7c0fd82e6ddd01c90ccb12b
Miguel Marin <miguelmmarin@viaro.net>
2005-09-14 16:34:16 +0000

    getting only the customer_id of the live_revision project

--- www/complaint-ae.tcl
+++ www/complaint-ae.tcl
@@ -20,7 +20,7 @@ ad_page_contract {
 
 if {![empty_string_p $project_id]} {
     if {[empty_string_p $customer_id]} {
-        set customer_id [db_string get_customer_id "select customer_id from pm_projectsx where item_id = :project_id"]
+        set customer_id [db_string get_customer_id "select p.customer_id from pm_projectsx p, cr_items i where p.item_id = :project_id and i.live_revision = p.revision_id"]
     }
     set object_id $project_id
 }
