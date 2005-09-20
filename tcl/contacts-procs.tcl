8251abe0b36f3ede790d6839c8fbcf08d97964c9
Malte Sussdorff <openacs@sussdorff.de>
2005-09-20 07:09:10 +0000

    Added contact::util::get_employee_organization

--- tcl/contacts-procs.tcl
+++ tcl/contacts-procs.tcl
@@ -65,7 +65,7 @@ ad_proc -private contact::util::get_file_extension {
     return [lindex [split $filename "."] end]
 }
 
-ad_proc -public contact::util::get_employees {
+ ad_proc -public contact::util::get_employees {
     {-organization_id:required}
 } {
     get employees of an organization
@@ -78,6 +78,27 @@ ad_proc -public contact::util::get_employees {
 	from acs_rels, acs_rel_types
 	where acs_rels.rel_type = acs_rel_types.rel_type
 	and ( object_id_one = :organization_id or object_id_two = :organization_id )
+	and acs_rels.rel_type = 'contact_rels_employment'
+    } {
+	lappend contact_list $other_party_id
+    }
+
+    return $contact_list
+}
+
+ad_proc -public contact::util::get_employee_organization {
+    {-employee_id:required}
+} {
+    get organization of an employee
+} {
+    set contact_list {}
+    db_foreach select_employee_ids {
+	select CASE WHEN object_id_one = :employee_id
+                    THEN object_id_two
+                    ELSE object_id_one END as other_party_id
+	from acs_rels, acs_rel_types
+	where acs_rels.rel_type = acs_rel_types.rel_type
+	and ( object_id_one = :employee_id or object_id_two = :employee_id )
 	and acs_rels.rel_type = 'contact_rels_employment'
     } {
 	lappend contact_list $other_party_id
