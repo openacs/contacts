ed686075ee5c643e2c59085b1de8887357886c1c
Malte Sussdorff <openacs@sussdorff.de>
2005-10-20 13:57:33 +0000

    Missing parameters for the tasks portlet

--- lib/tasks-tasks-portlet.tcl
+++ lib/tasks-tasks-portlet.tcl
@@ -1,4 +1,4 @@
-foreach optional_param {party_id query search_id tasks_interval page page_size page_flush_p tasks_orderby} {
+foreach optional_param {party_id query search_id tasks_interval page page_size page_flush_p tasks_orderby show_filters_p emp_f} {
     if {![info exists $optional_param]} {
 	set $optional_param {}
     }
