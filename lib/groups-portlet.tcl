e91cd2980af8d383343227a230d3880656bc937f
Malte Sussdorff <openacs@sussdorff.de>
2006-02-16 14:26:54 +0000

    Fixed URL for subsites

--- lib/groups-portlet.tcl
+++ lib/groups-portlet.tcl
@@ -5,5 +5,6 @@ foreach optional_param {hide_form_p} {
 }
 
 set portlet_layout [parameter::get -parameter "DefaultPortletLayout"]
+set package_url [ad_conn package_url]
 
-set groups_url "[site_node::get_package_url -package_key "contacts"]${party_id}/groups"
+set groups_url "${package_url}/${party_id}/groups"
