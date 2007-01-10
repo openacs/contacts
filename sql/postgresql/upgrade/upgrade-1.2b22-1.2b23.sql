e44234e6affc664f92fa907cf8e291692265d97e
Malte Sussdorff <openacs@sussdorff.de>
2007-01-10 07:36:34 +0000

    Removed the bad idea to change the foreign key constraint for contacts on user_preferences

--- sql/postgresql/upgrade/upgrade-1.2b22-1.2b23.sql
+++
@@ -1,2 +0,0 @@
-alter table user_preferences drop constraint user_prefs_user_id_fk;
-alter table user_preferences add constraint user_prefs_user_id_fk foreign key  (user_id) references parties (party_id);
