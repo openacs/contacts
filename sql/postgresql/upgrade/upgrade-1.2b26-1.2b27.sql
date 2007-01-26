2f5022f92dd1690fef5d0a496d3da796145e543a
Malte Sussdorff <openacs@sussdorff.de>
2007-01-26 04:37:40 +0000

    Bad idea, as this forces users to change the attribute...

--- sql/postgresql/upgrade/upgrade-1.2b26-1.2b27.sql
+++
@@ -1,9 +0,0 @@
--- 
--- packages/contacts/sql/postgresql/upgrade/upgrade-1.2b26-1.2b27.sql
--- 
--- @author <yourname> (<your email>)
--- @creation-date 2007-01-23
--- @cvs-id $Id$
---
-
-update acs_attributes set attribute_name = 'home_address' where attribute_name = 'visitaddress';
